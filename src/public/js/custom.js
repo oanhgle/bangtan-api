1e15452dc8b0105a83b04a7c9d5ae035e4561afb
Dev Mood <57423171+dev-mood@users.noreply.github.com>
2021-03-12 15:33:02 +0100

    refactor: Activating dark mode in the operating system

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -1,28 +1,59 @@
 (function ($) {
-
   "use strict";
 
-    // COLOR MODE
-    $('.color-mode').click(function(){
-        $('.color-mode-icon').toggleClass('active')
-        $('body').toggleClass('dark-mode')
-    })
+  // COLOR MODE
+  if (window.matchMedia("(prefers-color-scheme)").media === "not all") {
+    console.warn(
+      "Your browser does not support the `prefers-color-scheme` media query."
+    );
+  }
 
-    // HEADER
-    $(".navbar").headroom();
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-    // SMOOTHSCROLL
-    $(function() {
-      $('.nav-link, .custom-btn-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });  
+  const currentTheme = localStorage.getItem("theme");
 
-    // TOOLTIP
-    $('.social-links a').tooltip();
+  if (currentTheme) {
+    document.body.classList.toggle(currentTheme);
+  } else {
+    document.body.classList.toggle(
+      prefersDarkScheme.matches ? "dark" : "light"
+    );
+  }
 
+  $(".color-mode").click(function () {
+    $(".color-mode-icon").toggleClass("active");
+    if (prefersDarkScheme.matches) {
+      document.body.classList.toggle("light");
+      const theme = document.body.classList.contains("light")
+        ? "light"
+        : "dark";
+      localStorage.setItem("theme", theme);
+    } else {
+      document.body.classList.toggle("dark");
+      const theme = document.body.classList.contains("dark") ? "dark" : "light";
+      localStorage.setItem("theme", theme);
+    }
+  });
+
+  // HEADER
+  $(".navbar").headroom();
+
+  // SMOOTHSCROLL
+  $(function () {
+    $(".nav-link, .custom-btn-link").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
+      event.preventDefault();
+    });
+  });
+
+  // TOOLTIP
+  $(".social-links a").tooltip();
 })(jQuery);
