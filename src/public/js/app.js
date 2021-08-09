ffd35158f218c5201b56f4e569e187dd227cccd7
Eliut González <reliutg@gmail.com>
2021-08-09 17:31:28 +0200

    fix: tweet button
    the first time you clicked on the tweet button it didn't work

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,33 +1,42 @@
-/* fetch quotes */
-function getRandom()
-{
-    fetch('https://bts-quotes-api.herokuapp.com/quote/random')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('quote').innerHTML = data.quote;
-            document.getElementById('author').innerHTML = data.member;
-            console.log(data.quote + ' - ' + data.member); //for debugging
-            
-            getInfo(data.quote);
+function tweet() {
+  const quote = document.getElementById("quote").innerHTML;
+  const author = document.getElementById("author").innerHTML;
+  const info = document.getElementById("info").innerHTML;
+  window.open(
+    `https://twitter.com/intent/tweet?text=${quote} - ${author} (${info})&hashtags=BTSQUOTESAPI`
+  );
+}
 
-            //share on twitter
-            document.getElementById("shareOnTW").href = "http://twitter.com/intent/tweet/?text=" + encodeURIComponent(data.quote + ' - ' + data.member);
-        }).catch(err => console.log(err));
+/* fetch quotes */
+function getRandom() {
+  fetch("https://bts-quotes-api.herokuapp.com/quote/random")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("quote").innerHTML = data.quote;
+      document.getElementById("author").innerHTML = data.member;
+      console.log(data.quote + " - " + data.member); //for debugging
+
+      getInfo(data.quote);
+
+      //share on twitter
+      document.getElementById("shareOnTW").href =
+        "http://twitter.com/intent/tweet/?text=" +
+        encodeURIComponent(data.quote + " - " + data.member);
+    })
+    .catch((err) => console.log(err));
 }
 
 /* fetch quote's info */
-function getInfo(quote)
-{
-    fetch('https://bts-quotes-api.herokuapp.com/quotes')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(function(data){
-                if(data.quote === quote)
-                {
-                    document.getElementById('info').innerHTML = data.info;
-                    console.log(data.info); //for debugging
-                }
-            });
-        }).catch(err => console.log(err));
+function getInfo(quote) {
+  fetch("https://bts-quotes-api.herokuapp.com/quotes")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach(function (data) {
+        if (data.quote === quote) {
+          document.getElementById("info").innerHTML = data.info;
+          console.log(data.info); //for debugging
+        }
+      });
+    })
+    .catch((err) => console.log(err));
 }
-
